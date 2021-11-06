7bc7930fd11dd018ad29dc2999aed34fb8fed15a
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2021-11-07 03:07:06 +0900

    2021/11/07

--- Quiz5-5.py
+++ Quiz5-5.py
@@ -1,24 +1,25 @@
 import pyxel
 
-pyxel.init(200, 200)
+pyxel.init(200, 200)  # 画面の表示
 
 a = 0
 b = 0
+# 変数の設定
 
 
 def update():
     global a
     global b
-    b += 1
-    a += 10 + b
-    a = a % 200
+    b += 1  # 徐々にスピードを上げていく変数としてbを設定
+    a += 10 + b  # aが増えることで位置が移動する
+    a = a % 200  # 範囲外に円が出ないように設定している。
 
 
 def draw():
     global a
     global b
-    pyxel.cls(7)
-    pyxel.circ(a, a, 10, 0)
+    pyxel.cls(7)  # 背景色の指定。白色
+    pyxel.circ(a, a, 10, 0)  # 円の座標、および大きさと色をしている。
 
 
-pyxel.run(update, draw)
+pyxel.run(update, draw)  # 上の関数を動かす。
